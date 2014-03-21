135b2a2f136b212bddf35566300e511d32465ffb
hburn <hburnweb@gmail.com>
2014-03-21 11:29:34 +0800

    theme
    v1.0

---
+++ footer.php
@@ -0,0 +1,7 @@
+<!-- Footer -->
+	<p class="grid_12 footer clearfix"> <span class="float">版权所有 &copy; 2014 <?php bloginfo('name'); ?>&nbsp;&nbsp;|&nbsp;&nbsp;Code By <a href="http://www.hburnweb.com">Hburn</a></span> <a class="float right" href="#">top</a> </p>
+</div>
+<!--end wrapper-->
+<?php wp_footer(); ?>
+</body>
+</html>
